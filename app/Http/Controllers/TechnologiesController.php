8bf8070e68a7e94c62983436b722edf379c978e0
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2024-02-23 16:27:41 +0100

    add: function index

--- app/Http/Controllers/TechnologiesController.php
+++ app/Http/Controllers/TechnologiesController.php
@@ -6,5 +6,10 @@ use Illuminate\Http\Request;
 
 class TechnologiesController extends Controller
 {
-    //
+    public function index()
+    {
+
+        return view('pages.technologies.index');
+
+    }
 }
