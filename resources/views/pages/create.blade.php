92fc4a93a3cd03a6fe2f5b2be3021f78cf16201c
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2024-02-25 11:19:42 +0100

    add: form

--- resources/views/pages/create.blade.php
+++ resources/views/pages/create.blade.php
@@ -6,6 +6,36 @@
 
 @section('content')
 
+<h1>Create a new project</h1>
 
+<!-- FORM TO CREATE NEW PROJECT -->
+<form method="POST">
+
+    @csrf
+    @method('POST')
+
+    <label for="name">Project name</label>
+    <input type="text" name="name" id="name">
+
+    <br>
+
+    <label for="author">Project author</label>
+    <input type="text" name="author" id="author">
+
+    <br>
+
+    @foreach ($technologies as $technology)
+    <input type="checkbox" name="technology_id[]" id="{{ 'technology_id_' . $technology -> id }}"
+        value="{{ $technology -> id }}">
+    <label for="{{ 'technology_id_' . $technology -> id }}">
+        {{ $technology -> name }}
+    </label>
+    <br>
+
+    @endforeach
+
+    <input type="submit" value="CREATE">
+
+</form>
 
 @endsection
