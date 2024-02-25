3494e493002b26f6a243d2dfc4f1e458ce9e73b9
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2024-02-25 10:09:29 +0100

    show: data in page

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.main-layout')
 
 @section('head')
-<title>Home</title>
+<title>Projects</title>
 @endsection
 
 @section('content')
@@ -9,11 +9,21 @@
 <ul>
     @foreach ($projects as $project)
     <li>
+
         <h3>Project name: </h3>
         <span>{{ $project -> name }}</span>
         <br>
+
         <strong>Type name: </strong>
         <span>{{ $project -> type -> name}}</span>
+
+        <span>
+            <strong>Technology name: </strong>
+            @foreach ($project -> technologies as $technology)
+            #{{ $technology -> name }}
+            @endforeach
+        </span>
+
     </li>
     @endforeach
 </ul>
