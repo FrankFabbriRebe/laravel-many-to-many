7a2d74f881e1c87dbbd316a85d0b6624da797b61
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2024-02-25 10:11:59 +0100

    show: data in page

--- resources/views/pages/technologies/index.blade.php
+++ resources/views/pages/technologies/index.blade.php
@@ -10,10 +10,22 @@
 
 <ul>
     @foreach ($technologies as $technology)
+
     <li>
+
         <h3>Technology name: </h3>
         <span>{{ $technology -> name }}</span>
+
+
+        <span>
+            <strong>Projects associate: </strong>
+            @foreach ($technology -> projects as $project)
+            {{ $project -> name }}
+            @endforeach
+        </span>
+
     </li>
+
     @endforeach
 </ul>
 
