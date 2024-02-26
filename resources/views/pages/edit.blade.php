ae47290c4e57ddb755a6977f29e863d100b55056
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2024-02-26 16:36:11 +0100

    add form

--- resources/views/pages/edit.blade.php
+++ resources/views/pages/edit.blade.php
@@ -8,4 +8,51 @@
 
 <h1>Edit project</h1>
 
+<!-- FORM TO EDIT A PROJECT -->
+<form action="{{ route('route.update', $project -> id) }}" method="POST">
+
+    @csrf
+    @method('POST')
+
+    <label for="name">Name</label>
+    <input type="text" name="name" id="name" value="{{ $project -> name }}">
+
+    <br>
+
+    <label for="author">Author</label>
+    <input type="text" name="author" id="author" value="{{ $project -> author }}">
+
+    <br>
+
+    <label for="type_id">Type</label>
+    <select name="type_id" id="type_id">
+        @foreach ($types as $type)
+        <option value="{{ $type -> id }}" @if ($project -> type -> id == $type -> id)
+            selected
+            @endif
+            >{{ $type -> name }}</option>
+        @endforeach
+    </select>
+
+    <br>
+
+    @foreach ($technologies as $technology)
+    <div>
+        <input type="checkbox" name="technology_id[]" value="{{ $technology -> id }}" @foreach ($project -> technologies
+        as $pTech)
+        @checked($pTech -> id === $technology -> id)
+        @endforeach
+        >
+        <label>
+            {{ $technology -> name }}
+        </label>
+    </div>
+    @endforeach
+
+    <br>
+
+    <input type="submit" value="UPDATE">
+
+</form>
+
 @endsection
