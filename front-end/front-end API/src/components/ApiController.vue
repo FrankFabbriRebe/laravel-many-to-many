01bb349b3e94cb0e212db212ce00d3b9f70270d0
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2024-02-28 16:49:43 +0100

    call API and show data

--- front-end/front-end API/src/components/ApiController.vue
+++ front-end/front-end API/src/components/ApiController.vue
@@ -1,3 +1,37 @@
+<script>
+
+import axios from 'axios';
+
+export default {
+    name: 'ApiContent',
+    data() {
+        return {
+            technologies: [],
+        }
+    },
+
+    mounted() {
+        axios.get('http://127.0.0.1:8000/api/v1/technologies')
+            .then(res => {
+                const data = res.data;
+                this.technologies = data.technologies;
+            })
+            .catch(error => {
+                console.error(error)
+            })
+    }
+
+}
+
+</script>
+
+
 <template>
-    <h1>ApiController</h1>
+    <div class="container">
+        <h1>List of technologies:</h1>
+        <div class="techno" v-for="technology in technologies">
+            <h2>{{ technology.name }}</h2>
+            <p>{{ technology.description }}</p>
+        </div>
+    </div>
 </template>
