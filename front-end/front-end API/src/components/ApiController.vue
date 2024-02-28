1eaf71ecc59aed9221467d6644722d01006b0dd5
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2024-02-28 17:24:25 +0100

    add form to create new technology

--- front-end/front-end API/src/components/ApiController.vue
+++ front-end/front-end API/src/components/ApiController.vue
@@ -7,6 +7,11 @@ export default {
     data() {
         return {
             technologies: [],
+            createFormActive: false,
+            newTechnology: {
+                name: '',
+                description: '',
+            }
         }
     },
 
@@ -19,6 +24,14 @@ export default {
             .catch(error => {
                 console.error(error)
             })
+    },
+
+    methods: {
+
+        toggleCreateNewTechnology() {
+            this.createFormActive = true;
+        }
+
     }
 
 }
@@ -28,12 +41,34 @@ export default {
 
 <template>
     <div class="container">
-        <h1>List of technologies:</h1>
-        <div class="techno" v-for="technology in technologies">
-            <h2>Name of technology: </h2>
-            <p>{{ technology.name }}</p>
-            <h3>Description of tecnìhnology: </h3>
-            <p>{{ technology.description }}</p>
+
+        <h1>Technologies:</h1>
+
+        <!-- form to add new technology -->
+        <form v-if="createFormActive">
+            <label for="name">name</label>
+            <br>
+            <input type="text" name="name" id="name" v-model="newTechnology.name">
+            <br>
+            <label for="description">description</label>
+            <br>
+            <input type="text" name="description" id="description" v-model="newTechnology.description">
+            <br>
+            <input type="submit" value="CREATE">
+        </form>
+
+        <div v-else>
+
+            <button @click="toggleCreateNewTechnology">CREATE NEW TECHNOLOGY</button>
+
+            <div class="techno" v-for="technology in technologies">
+                <h2>Name of technology: </h2>
+                <p>{{ technology.name }}</p>
+                <h3>Description of tecnìhnology: </h3>
+                <p>{{ technology.description }}</p>
+            </div>
+
         </div>
+
     </div>
 </template>
